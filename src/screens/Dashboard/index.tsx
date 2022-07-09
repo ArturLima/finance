d533050851c9fbc8933415806a09c4d31dee2092
Artur <arturpeixotoo@gmail.com>
2022-07-09 12:59:53 -0300

    include cards

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { HighlightCard } from "../../Components/HighlightCard";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "http://github.com/arturlima.png" }} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Artur</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+          type="total"
+        />
+      </HighlightCards>
+    </Container>
+  );
+}
